704f86f2808e045ab730ca770ea8162683d11151
dansan-dsn <ddryn970@gmail.com>
2024-11-27 16:04:06 +0300

    bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,23 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Chat from "./pages/chat";
-import Register from "./pages/register";
-import Login from "./pages/login";
+import Chat from "./pages/Chat";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Chat />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Navigate to="/" />}></Route>
-      </Routes>
+      <NavBar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Chat />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/" />}></Route>
+        </Routes>
+      </Container>
     </>
   );
 }
