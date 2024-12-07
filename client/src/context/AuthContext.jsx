3c00b6b8a55e0aa45cc6dfb7c79a44ae7b4da1f9
dansan-dsn <ddryn970@gmail.com>
2024-12-07 16:20:27 +0300

    abit

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,85 @@
+import { createContext, useCallback, useState, useEffect } from "react";
+import { baseUrl, postRequest } from "../utils/services";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [registerError, setRegisterError] = useState(null);
+  const [isRegisterLoading, setIsRegisterLoading] = useState(false);
+  const [registerInfo, setRegisterInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [loginInfo, setLoginInfo] = useState({
+    email: "",
+    password: "",
+  });
+  const [loginError, setLoginError] = useState(null);
+  const [isLoginLoading, setIsLoginLoading] = useState(false);
+
+  console.log("User", user);
+
+  useEffect(() => {
+    const user = localStorage.getItem("User");
+
+    setUser(JSON.parse(user));
+  }, []);
+
+  const updateRegisterInfo = useCallback((info) => {
+    setRegisterInfo(info);
+  }, []);
+
+  const registerUser = useCallback(
+    async (e) => {
+      e.preventDefault();
+
+      setIsRegisterLoading(true);
+      setRegisterError(null);
+
+      const response = await postRequest(
+        `${baseUrl}/users/register`,
+        JSON.stringify(registerInfo)
+      );
+
+      setIsRegisterLoading(false);
+
+      if (response.error) {
+        return setRegisterError(response);
+      }
+
+      localStorage.setItem("User", JSON.stringify(response));
+      setUser(response);
+    },
+    [registerInfo]
+  );
+
+  const loginUser = useCallback(async () => {
+    const response = await postRequest(
+      `${baseUrl}/users/login`,
+      JSON.stringify(loginInfo)
+    );
+  }, [loginInfo]);
+
+  const logoutUser = useCallback(() => {
+    localStorage.removeItem("User");
+    setUser(null);
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        registerInfo,
+        updateRegisterInfo,
+        registerUser,
+        registerError,
+        isRegisterLoading,
+        logoutUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
