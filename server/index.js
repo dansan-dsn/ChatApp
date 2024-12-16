2b4a4d395edfc890e89775f8fee164b3f86e61a4
dansan-dsn <ddryn970@gmail.com>
2024-12-16 18:18:47 +0300

    ddd

--- server/index.js
+++ server/index.js
@@ -1,25 +1,27 @@
-require('dotenv').config() // configureing .env
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const port = process.env.PORT || 5000
-const uri = process.env.DB_URI
-const userRoute = require('./routes/userRoute')
+require("dotenv").config(); // configureing .env
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const port = process.env.PORT || 5000;
+const uri = process.env.DB_URI;
+const userRoute = require("./routes/userRoute");
+const chatRoute = require("./routes/chatRoute");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
-app.use('/api/users', userRoute)
+app.use(express.json());
+app.use(cors());
+app.use("/api/users", userRoute);
+app.use("/api/chats", chatRoute);
 
 app.listen(port, (req, res) => {
-    console.log(`Server running on port: ${port}`)
-})
+  console.log(`Server running on port: ${port}`);
+});
 
 mongoose
-.connect(uri, {
+  .connect(uri, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(()=> console.log('Database connected'))
-.catch((err) => console.log('Database Failed', err.message))
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log("Database Failed", err.message));
